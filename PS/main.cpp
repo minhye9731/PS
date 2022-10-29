7eaa0dc52353cc69b067eff22a2fcf7442bbb981
minhye9731 <jenney62639731@gmail.com>
2022-10-29 09:29:38 +0900

    Initial Commit

---
+++ PS/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  PS
+//
+//  Created by 강민혜 on 10/29/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
